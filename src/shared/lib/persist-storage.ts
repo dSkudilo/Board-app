13f1cb94bc18b7035c7672a5100590e29e4dacfc
dSkudilo <dskudilo@gmail.com>
2025-04-25 20:17:07 +0300

    helpers db

---
+++ src/shared/lib/persist-storage.ts
@@ -0,0 +1,29 @@
+import { getItem, setItem } from "localforage";
+
+const makeError = (e:unknown) => {
+  if (e instanceof Error) {
+    console.error(e.message);
+    throw e;
+  } else {
+    throw new Error("Неизвестная ошибка");
+  }
+}
+
+export const persistStorage = {
+  getItemsSafe: async <T>(key: string, defaultValue: T): Promise<T | undefined> => {
+    try {
+      const res = await getItem<T>(key)
+      return res || defaultValue;
+    } catch (e: unknown) {
+      makeError(e)
+    }
+  },
+  
+  setItemsSafe:async <T>(key: string, value: T) => {
+    try {
+      return await setItem<T>(key, value);
+    } catch (e: unknown) {
+      makeError(e)
+    }
+  },
+}
