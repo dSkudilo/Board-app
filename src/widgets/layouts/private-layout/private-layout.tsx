cd6a6754c56aff634cccae79fb874ba155404058
dSkudilo <dskudilo@gmail.com>
2025-05-20 23:45:37 +0300

    fix

--- src/widgets/layouts/private-layout/private-layout.tsx
+++ src/widgets/layouts/private-layout/private-layout.tsx
@@ -1,12 +1,13 @@
 import { UiContainer, UiHeader, UiLogo, UiNav } from '@/shared/ui';
 import { UiToaster } from '@/shared/ui/ui-toaster';
+import { LayoutWrapper } from '@/widgets/layouts/private-layout/ui/layout-wrapper';
 
 export function PrivateLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen flex flex-col">
+    <LayoutWrapper>
       <UiHeader left={<UiLogo />} navBar={<UiNav />} right={<div>right</div>} />
       <UiContainer>{children}</UiContainer>
       <UiToaster />
-    </div>
+    </LayoutWrapper>
   );
 }
